6db50bb3e47441116239ab1157008ae87ae93f9e
Cecil1999 <Cecil2090@gmail.com>
2025-11-11 20:30:42 -0500

    24 > 8, prefer 8 as that's one session over a work shift.

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,7 +4,7 @@ module JsonWebToken
   # Need to figure out a good way to 'Store' secret_key for the signature. For now.... just use the test Key.
   @hmac_secret = "kWnVtZA2LezioE6c9TvP0ZRyT7mydZLKG49MafhX9GE="
   # Self Encoding
-  def self.encode(payload, exp = 24.hours.to_i)
+  def self.encode(payload, exp = 8.hours.to_i)
     payload[:exp] = Time.now.to_i + exp
     payload[:jid] = ActiveRecord::Base.connection.exec_query("SELECT nextval('jwt_id_sequence')").to_a.first["nextval"]
     JWT.encode(payload, @hmac_secret, "HS256")
