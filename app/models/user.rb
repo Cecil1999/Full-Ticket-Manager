c8cf2294f91a535c588d5687c212ebe0a8b3657f
Cecil1999 <Cecil2090@gmail.com>
2025-11-14 22:44:10 -0500

    Start on roles modal.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  belongs_to_and_has_many :role
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_11_231617) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_15_021605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,11 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_11_231617) do
     t.bigint "ticket_id"
     t.datetime "updated_at", null: false
     t.index ["ticket_id"], name: "index_posts_on_ticket_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.index ["name"], name: "index_roles_on_name", unique: true
   end
 
   create_table "ticket_types", force: :cascade do |t|
@@ -37,6 +42,13 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_11_231617) do
     t.index ["ticket_type_id"], name: "index_tickets_on_ticket_type_id"
   end
 
+  create_table "user_roles", id: false, force: :cascade do |t|
+    t.bigint "role_id"
+    t.bigint "user_id"
+    t.index ["role_id"], name: "index_user_roles_on_role_id"
+    t.index ["user_id"], name: "index_user_roles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", null: false
@@ -48,4 +60,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_11_231617) do
 
   add_foreign_key "posts", "tickets"
   add_foreign_key "tickets", "ticket_types"
+  add_foreign_key "user_roles", "roles"
+  add_foreign_key "user_roles", "users"
 end
