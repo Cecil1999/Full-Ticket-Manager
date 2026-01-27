f06e08e36bdf9d014a7a700fff901a54d1f070ca
Cecil1999 <Cecil2090@gmail.com>
2026-01-27 18:28:43 -0500

    fix Tickets/Posts.

--- react-frontend/src/TicketPost.tsx
+++ react-frontend/src/TicketPost.tsx
@@ -3,7 +3,7 @@ import type { Post } from "./types/Post";
 export function TicketPost({ body, user }: Post) {
   return <>
     <div className="border border-1 rounded-xl p-4">
-      {body} - {user.username}
+      {body} - {user?.username}
     </div>
   </>
 }
