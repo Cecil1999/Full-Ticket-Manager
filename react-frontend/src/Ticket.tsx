a1c19a0b6ec1095f230a0303e8d31da61e3e7027
Cecil1999 <Cecil2090@gmail.com>
2026-02-10 19:14:39 -0500

    Fix scrolling issue.

--- react-frontend/src/Ticket.tsx
+++ react-frontend/src/Ticket.tsx
@@ -25,17 +25,19 @@ function PostsThread({ posts, addPosts }: PostsThreadProps) {
   }
 
   return <>
-    <div className="flex-grow flex flex-col overflow-y-auto gap-4 p-4">
-      {optimisticPosts ? optimisticPosts.map((o, i) => <TicketPost key={i} body={o.body} user={o.user} />) : null}
-    </div>
-    <div className="sticky h-fit bottom-0 border-t-1 rounded-xl shadow-md bg-white p-2">
-      <form method="POST" action={postsAction}>
-        <label htmlFor="new_post">New Post</label>
-        <textarea className="w-full p-2 rounded-md resize-none bg-gray-200 focus:outline-none focus:border-blue-500" rows={4} placeholder="Add to the ticket" name="new_post" id="new_post" required></textarea>
-        <div className="p-4">
-          <button type="submit" className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg">Submit</button>
-        </div>
-      </form>
+    <div className="flex-grow flex flex-col h-full gap-4">
+      <div className="overflow-y-auto min-h-[calc(100%-350px)] xl:min-h-[calc(100% - 300px)] p-4">
+        {optimisticPosts ? optimisticPosts.map((o, i) => <TicketPost key={i} body={o.body} user={o.user} />) : null}
+      </div>
+      <div className="flex-1 min-h-[350px] border-t-1 rounded-xl shadow-md bg-white p-2 xl:min-h-[300px]">
+        <form method="POST" action={postsAction}>
+          <label htmlFor="new_post">New Post</label>
+          <textarea className="w-full p-2 rounded-md resize-none bg-gray-200 focus:outline-none focus:border-blue-500" rows={4} placeholder="Add to the ticket" name="new_post" id="new_post" required></textarea>
+          <div className="p-4">
+            <button type="submit" className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   </>
 }
@@ -107,7 +109,7 @@ export function DisplayTicket() {
           ? (<div>Choose Ticket...</div>)
           : (
             <div className="flex flex-col h-screen overflow-hidden">
-              <div className="sticky h-fit top-0 bg-white">
+              <div className="sticky h-fit z-10 top-0">
                 <div className="text-xl py-1 px-2 border-b-1 rounded-xl shadow-md">
                   <h2 className="text-center text-3xl font-semibold p-2">
                     {ticketData?.title} - {ticketData?.ticket_type.name}
@@ -124,7 +126,9 @@ export function DisplayTicket() {
                 </div>
               </div>
               <div className="hidden" id="ticket_posts_errors"></div>
-              <PostsThread posts={PostData} addPosts={addPost} />
+              <div className="flex-grow overflow-y-auto">
+                <PostsThread posts={PostData} addPosts={addPost} />
+              </div>
             </div>
           )
         }
